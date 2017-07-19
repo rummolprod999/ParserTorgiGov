a18109df811e9080c791086a60c62452fd02ed56
rummolprod999 <rummolprod999@gmail.com>
2017-07-19 09:45:54 +0300

    Initial commit

---
+++ ParserTorgiGov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParserTorgiGov
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
