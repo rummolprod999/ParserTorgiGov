9db7483f3f1659a09f759340e1dc3a629ab798a0
rummolprod999 <rummolprod999@gmail.com>
2017-07-25 11:57:44 +0300

    update

--- ParserTorgiGov/Torg.cs
+++ ParserTorgiGov/Torg.cs
@@ -8,12 +8,14 @@ namespace ParserTorgiGov
         protected readonly string publishDate;
         protected readonly string lastChanged;
         protected readonly int isArchived;
-        public Torg(string bidNumber, string publishDate, string lastChanged, int isArchived)
+        protected readonly int b;
+        public Torg(string bidNumber, string publishDate, string lastChanged, int isArchived, int b)
         {
             this.bidNumber = bidNumber;
             this.publishDate = publishDate;
             this.lastChanged = lastChanged;
             this.isArchived = isArchived;
+            this.b = b;
         }
 
         public virtual void Parse()
--- ParserTorgiGov/TorgBidKind1.cs
+++ ParserTorgiGov/TorgBidKind1.cs
@@ -2,7 +2,7 @@
 {
     public class TorgBidKind1:Torg
     {
-        public TorgBidKind1(string bidNumber, string publishDate, string lastChanged, int isArchived):base(bidNumber, publishDate, lastChanged, isArchived)
+        public TorgBidKind1(string bidNumber, string publishDate, string lastChanged, int isArchived, int b):base(bidNumber, publishDate, lastChanged, isArchived, b)
         {
             
         }
